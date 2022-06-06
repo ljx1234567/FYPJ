12b4afec2850e4841029ea05a03a5e9507ccf41f
Nigel-N <nigellim20>
2022-06-06 11:01:21 +0800

    call api from backend

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,30 @@
+import React, {useEffect, useState} from 'react'
+
 function App() {
-    return (
-      <div className="App">
-        <h1>Home</h1>
-      </div>
-    );
-  }
-  
-  export default App;
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
+  return(
+    <div>
+    {(typeof backendData.users === "undefined") ? (
+      <p>Loading ...</p>
+    ): (
+      backendData.users.map((user, i) => (
+        <p key={i}>{user}</p>
+      ))
+    )}
+    </div>
+  )
+}
+
+export default App;
